5020f77b61e7c6874fc760c11282c2c28f5ea2ac
cterrasid <clarette.terrasi@gmail.com>
2019-06-30 01:52:02 +0200

    delete date value

--- src/components/InputDate/index.js
+++ src/components/InputDate/index.js
@@ -3,25 +3,18 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const InputDate = props => {
-  const { date, handleDateInput } = props;
+  const { handleDateInput } = props;
 
   return (
     <label htmlFor="date" title="date">
       Date
-      <input
-        type="date"
-        id="date"
-        name="date"
-        value={date}
-        onChange={handleDateInput}
-      />
+      <input type="date" id="date" name="date" onChange={handleDateInput} />
     </label>
   );
 };
 
 InputDate.propTypes = {
   handleDateInput: PropTypes.func.isRequired,
-  date: PropTypes.string.isRequired,
 };
 
 export default InputDate;
